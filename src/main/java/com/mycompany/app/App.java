d79a8105f44a612db1feaa3f9741a37824986e4b
Ugur Tufekci <ugurtufekci.tobbetu@gmail.com>
2017-01-27 20:33:51 +0300

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,62 @@
 
 package com.mycompany.app;
 
+import static spark.Spark.*;
+
+import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App{
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	 get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+
+          boolean result = App.search(inputList, input2AsInt);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
     }
+    
 public static boolean search(ArrayList<Integer> array, int e) {
         System.out.println("inside search");
         if (array == null) return false;
